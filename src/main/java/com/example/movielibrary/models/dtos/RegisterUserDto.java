05d99c24fa6b5f8605ba0378c34d39e6e9dca48e
Aleksandar <aleksandar.oynakov@gmail.com>
2026-07-20 15:38:10 +0300

    Added validations impl in build.gradle and created RegisterUserDto

---
+++ src/main/java/com/example/movielibrary/models/dtos/RegisterUserDto.java
@@ -0,0 +1,30 @@
+package com.example.movielibrary.models.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterUserDto {
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
