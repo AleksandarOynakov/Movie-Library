6c6ca6a3a384699497b4f37ecc53467a6d34e9f3
Aleksandar <aleksandar.oynakov@gmail.com>
2026-07-20 17:07:16 +0300

    Created ResponseUserDto and register post method in UserRestController

--- src/main/java/com/example/movielibrary/controllers/UserRestController.java
+++ src/main/java/com/example/movielibrary/controllers/UserRestController.java
@@ -1,10 +1,28 @@
 package com.example.movielibrary.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.movielibrary.models.dtos.RegisterUserDto;
+import com.example.movielibrary.models.dtos.ResponseUserDto;
+import com.example.movielibrary.models.user.User;
+import com.example.movielibrary.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class UserRestController {
+    private final UserService userService;
 
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseUserDto register(@Valid @RequestBody RegisterUserDto registerUserDto){
+        User user = userService.register(registerUserDto);
+        return new ResponseUserDto(user.getId(), user.getUsername(), user.getRole());
+    }
 }
