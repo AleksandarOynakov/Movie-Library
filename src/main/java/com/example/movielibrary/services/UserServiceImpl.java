67ff68e18093ba7373faf185d2e3637f425a724e
Aleksandar <aleksandar.oynakov@gmail.com>
2026-07-20 16:14:12 +0300

    Added data-jpa and security to build.gradle and started creating UserServiceImpl

---
+++ src/main/java/com/example/movielibrary/services/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.movielibrary.services;
+
+import com.example.movielibrary.models.dtos.RegisterUserDto;
+import com.example.movielibrary.models.user.User;
+import com.example.movielibrary.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    public User register(RegisterUserDto registerUserDto){
+//
+//    }
+}
