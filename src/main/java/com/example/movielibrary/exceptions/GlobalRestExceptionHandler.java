aeb40065dbe0a1d629c12f0c6f5f59137fe72c12
Aleksandar <aleksandar.oynakov@gmail.com>
2026-07-20 18:29:21 +0300

    Added usernameNotFoundExceptionHandler in GlobalRestExceptionHandler

--- src/main/java/com/example/movielibrary/exceptions/GlobalRestExceptionHandler.java
+++ src/main/java/com/example/movielibrary/exceptions/GlobalRestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.movielibrary.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,12 @@ public class GlobalRestExceptionHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public String duplicateEntityExceptionHandler(DuplicateEntityException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String usernameNotFoundExceptionHandler(UsernameNotFoundException e){
         return e.getMessage();
     }
 
