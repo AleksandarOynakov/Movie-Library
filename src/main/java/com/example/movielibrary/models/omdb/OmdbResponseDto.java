f40e4a0f083c3d1580041bef2e8e53e943fda35f
Aleksandar <aleksandar.oynakov@gmail.com>
2026-07-21 14:51:50 +0300

    Created omdbResponseDto

---
+++ src/main/java/com/example/movielibrary/models/omdb/OmdbResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.movielibrary.models.omdb;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record OmdbResponseDto(
+        @JsonProperty("Title") String title,
+        @JsonProperty("Year") String year,
+        @JsonProperty("imdbRating") String imdbRating,
+        @JsonProperty("Response") String response,
+        @JsonProperty("Error") String error
+) {
+    public boolean wasFound() {
+        String trueResponse = "True";
+        return trueResponse.equalsIgnoreCase(response);
+    }
+}
